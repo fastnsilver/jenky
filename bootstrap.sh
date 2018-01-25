c15a3cd1263577eeec7ea32d0636d904f9fe41c8
Chris Phillipson <fastnsilver@gmail.com>
2018-01-25 11:24:57 -0800

    Add Sonarqube * Rev versions of software in docker-compose.yml to latest available * Separate volumes for each service

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 echo "JENKINS_SLAVE_SSH_PUBKEY="`cat ~/.ssh/jenky_rsa.pub` > slave.env
 
 docker-compose build
-docker-compose -f docker-compose-with-artifactory.yml up -d
+docker-compose up -d
