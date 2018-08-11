437023bc5448f734b68f7deea566333f16b9eed9
Chris Phillipson <fastnsilver@gmail.com>
2018-08-11 16:31:48 -0700

    Update versions of software products * Artifactory to 6.2.0 * Postgres to 10.5 * Jenkins to 2.136 * Concourse to 4.0.0 * Java to latest JDK 8 release

--- compose/prepare-docker-vm.sh
+++ compose/prepare-docker-vm.sh
@@ -33,6 +33,6 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
